7f55d9852b6d4d111ee6d17fb17ccccc61bd6e79
Sergio Vega <hvega.sergio@gmail.com>
2018-08-31 12:38:48 -0600

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { Route } from "@angular/router";
 import { DashComponent } from "./dash/dash.component";
 import { FormComponent } from "./form/form.component";
 import { ListComponent } from "./list/list.component";
+import { PostDetailComponent } from "./post-detail/post-detail.component";
 
 export const routes: Route[] = [
     { path: '', component: DashComponent, outlet: 'dashboard' },
     { path: '', redirectTo: 'list', pathMatch: 'full' }, 
     { path: 'list', component: ListComponent },
-    { path: 'create', component: FormComponent}
+    { path: 'create', component: FormComponent},
+    { 
+        path: 'post', 
+        children: [
+            { path: ':id', component: PostDetailComponent }
+        ]
+    }
+
+
 ];
