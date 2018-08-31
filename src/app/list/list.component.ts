b8c93321dd3292fd74d3a2a94f850a0eee297f64
Sergio Vega <hvega.sergio@gmail.com>
2018-08-31 12:37:20 -0600

    update list component

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PostsService } from "../posts.service";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"]
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+	posts: Array<any>;
 
-  ngOnInit() {
-  }
+	constructor(private postsService: PostsService) {}
 
+	ngOnInit() {
+		this.getPosts();
+	}
+
+	getPosts(){
+
+		this.postsService.getPosts().subscribe(
+			(data: Array<any>) => {
+				this.posts = data.slice(0, 5);	
+			},
+			(err: HttpErrorResponse) => {
+				if (err.error instanceof Error) {
+					console.log('Client-Side error:', err.error.message);
+				} else {
+					console.log(`Backend error: codigo [ ${err.status} ], error: [ ${err.error} ]`);
+				}
+			}
+		);
+
+	}
 }
