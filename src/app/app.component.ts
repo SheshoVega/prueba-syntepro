c8d5a486f197c1cf559e007ce261ec21bf06b6ca
Sergio Vega <hvega.sergio@gmail.com>
2018-08-31 20:48:52 -0600

    adding responsive & refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'app';
+
+	slide:boolean;
+
+	toSlide() {
+		this.slide = !this.slide;
+	}
 }
