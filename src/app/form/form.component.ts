19469683900563aa0bfe8f6e020475ad3d89394a
Sergio Vega <hvega.sergio@gmail.com>
2018-08-31 18:48:04 -0600

    update form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PostsService } from '../posts.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +9,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+	@Input() isEditing:boolean;
+	@Input() post:any;
+	postForm: FormGroup;
+	showModal:boolean;
 
-  ngOnInit() {
-  }
+	constructor(private formBuilder: FormBuilder, private projectsService: PostsService) { }
+
+	ngOnInit() {
+		// console.log(this.post);
+
+		let titleDefault = '';
+		let bodyDefault = '';
+
+		if(this.isEditing) {
+
+			titleDefault = this.post.title;
+			bodyDefault = this.post.body;
+
+		}
+
+		this.postForm = this.formBuilder.group({
+			title: [titleDefault, Validators.required],
+			body: [bodyDefault, Validators.required]
+		});
+		
+	}
+
+	toModal() {
+
+		this.showModal = true;
+		
+	}
+
+
+	savePost() {
+
+		console.log('guardar');
+		
+	}
+
+	updatePost() {
+		console.log('actualizar');
+	}
+
+	resetForm() {
+
+		this.postForm.reset();
+
+	}
+
+	getModalAction(data:any) {
+		if(data.continue){
+			
+			this.showModal = false;
+
+			if(this.isEditing) {
+
+				this.updatePost();
+
+			} else {
+
+				this.savePost();
+
+			}
+			
+		} else {
+			
+			this.showModal = false;			
+			
+		}
+	}
 
 }
